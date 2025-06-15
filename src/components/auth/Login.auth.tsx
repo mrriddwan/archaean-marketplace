a5e639723cd3ceeb394eb3b3c4c47231e444826e
Amir Ridwan <amirridwan.dev@gmail.com>
2025-06-15 15:07:44 +0800

    feat(Login): implement Google login functionality with loading state and applied authHook

--- src/components/auth/Login.auth.tsx
+++ src/components/auth/Login.auth.tsx
@@ -1,17 +1,37 @@
 import { FcGoogle } from "react-icons/fc";
+import { useAuthHook } from "../../hooks/authHook";
 
 export const Login = () => {
+  const { googleLogin, setIsAuthenticating, isAuthenticating } = useAuthHook();
+
   return (
     <div className="min-h-screen min-w-screen flex items-center justify-center bg-gray-100">
       <div className="w-96 p-8 bg-white rounded-lg shadow-lg">
         <div className="text-center mb-8">
-          <h1 className="text-3xl font-bold text-gray-800 mb-2">Welcome to NuMarket</h1>
+          <h1 className="text-3xl font-bold text-gray-800 mb-2">
+            Welcome to NuMarket
+          </h1>
           <p className="text-gray-600">Sign in to continue</p>
         </div>
 
-        <button className="w-full flex items-center justify-center gap-3 px-4 py-3 bg-white border border-gray-300 rounded-lg hover:bg-gray-50 transition-colors shadow-sm hover:scale-105">
-          <FcGoogle />
-          <span className="text-gray-300 font-medium">Continue with Google</span>
+        <button
+          className="w-full flex items-center justify-center gap-3 px-4 py-3 bg-white border border-gray-300 rounded-lg hover:bg-gray-50 transition-colors shadow-sm hover:scale-105"
+          disabled={isAuthenticating}
+          onClick={() => {
+            googleLogin();
+            setIsAuthenticating(true);
+          }}
+        >
+          {isAuthenticating ? (
+            <div>Loading ...</div>
+          ) : (
+            <div className="flex gap-3 items-center justify-center">
+              <FcGoogle />
+              <span className="text-gray-300 font-medium">
+                Continue with Google
+              </span>
+            </div>
+          )}
         </button>
 
         <div className="mt-6 text-center">
