341240b49e18bda08a39ede911bbf956b60c646b
Amir Ridwan <amirridwan.dev@gmail.com>
2025-06-17 23:05:21 +0800

    refactor(App): reorganize component structure and integrate QueryClientProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import "./App.css";
-import { Login } from "./components/auth/Login";
+// import { Login } from "./components/auth/Login";
 import { UserContextProvider } from "./contexts/userContext";
+import { ProductMain } from "./components/product/ProductMain";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 function App() {
   const googleClientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
+  const queryClient = new QueryClient();
   return (
     <GoogleOAuthProvider clientId={googleClientId}>
-      <UserContextProvider>
-        <Login />
-      </UserContextProvider>
+      <QueryClientProvider client={queryClient}>
+        <UserContextProvider>
+          {/* <Login /> */}
+          <ProductMain />
+        </UserContextProvider>
+      </QueryClientProvider>
     </GoogleOAuthProvider>
   );
 }
