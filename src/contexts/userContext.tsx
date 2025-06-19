42fd6333b3c5ab2ebeef0357ff55430d6d401f68
Amir Ridwan <amirridwan.dev@gmail.com>
2025-06-19 10:15:55 +0800

    feat(userContext): implement user authentication logic with token handling and user data fetching

--- src/contexts/userContext.tsx
+++ src/contexts/userContext.tsx
@@ -1,4 +1,6 @@
-import { createContext, useContext, useMemo, useState } from "react";
+import { createContext, useContext, useEffect, useMemo, useState } from "react";
+import Cookies from "js-cookie";
+import { authService } from "../services/auth.service";
 
 interface IUser {
   id: string | null;
@@ -16,16 +18,16 @@ interface IUserContextValue {
   setUserContext: React.Dispatch<React.SetStateAction<IUser>>;
 }
 
-export const initialUserContext = {
-    id: null,
-    email: "",
-    name: "",
-    picture: "",
-    given_name: "",
-    family_name: "",
-    email_verified: "",
-    isAuthenticated: false,
-  }
+export const initialUserContext: IUser = {
+  id: null,
+  email: "",
+  name: "",
+  picture: "",
+  given_name: "",
+  family_name: "",
+  email_verified: "",
+  isAuthenticated: false,
+};
 
 const UserContext = createContext<IUserContextValue>({
   userContext: initialUserContext,
@@ -36,6 +38,35 @@ export const UserContextProvider: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
   const [user, setUser] = useState<IUser>(initialUserContext);
+
+  useEffect(() => {
+    const accessToken = Cookies.get("accessToken");
+
+    if (accessToken) {
+      
+      authService
+        .getUserGoogleInfo(accessToken)
+        .then((res) => {
+          if (res.data) {
+            const userData = res.data;
+            setUser({
+              id: userData.sub,
+              email: userData.email,
+              name: userData.name,
+              picture: userData.picture,
+              given_name: userData.given_name,
+              family_name: userData.family_name,
+              email_verified: userData.email_verified,
+              isAuthenticated: true,
+            });
+          }
+        })
+        .catch((err) => {
+          console.error("Invalid token or failed to fetch user info", err);
+          setUser(initialUserContext);
+        });
+    }
+  }, []);
 
   const contextValue = useMemo(
     () => ({ userContext: user, setUserContext: setUser }),
